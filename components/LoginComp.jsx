73faae2423c491a2d5279e5690fc2ed9ee3451f7
Erfan <erfanaliyavari@gmail.com>
2024-07-25 18:11:52 +0330

    fixed provider page

--- components/LoginComp.jsx
+++ components/LoginComp.jsx
@@ -1,16 +1,21 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { getProviders } from "next-auth/react";
+import { useEffect, useState } from "react";
 const LoginComp = () => {
-  const provider = async () => {
-    const { google } = await getProviders();
-    return google;
-  };
+  const [provider, setProvider] = useState();
+  useEffect(() => {
+    const provider = async () => {
+      const { google } = await getProviders();
+      setProvider(google.id);
+    };
+    provider();
+  }, []);
 
   return (
     <div className="flex flex-col w-full max-w-[20rem] mt-[-4rem] justify-center items-center border-2 shadow-xl rounded-xl px-4 py-16 gap-y-4">
       <button
-        onClick={() => signIn(provider().id)}
+        onClick={() => signIn(provider)}
         className="bg-black text-white px-4 py-2 rounded-md w-full"
       >
         Google
