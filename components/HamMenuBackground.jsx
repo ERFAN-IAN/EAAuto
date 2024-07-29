b601c81095ecb765abbe4dacd1457670140b9d38
Erfan <erfanaliyavari@gmail.com>
2024-07-29 09:46:10 +0330

    fixed hammenu background issue

--- components/HamMenuBackground.jsx
+++ components/HamMenuBackground.jsx
@@ -1,15 +1,15 @@
 import { useGlobalContext } from "@/context/context";
 const HamMenuBackground = () => {
-  const { hamMenu, setHamMenu } = useGlobalContext();
+  const { allStates, setAllStates } = useGlobalContext();
   return (
     <div
       className={` fixed top-[4.25rem] left-0 w-full h-[95%] transition-all duration-300 md:hidden ${
-        hamMenu
+        allStates.hamMenu
           ? " backdrop-brightness-50 test z-[11] opacity-100"
           : "backdrop-brightness-100 testt z-[-10] opacity-0"
       }`}
       onClick={() => {
-        setHamMenu(false);
+        setAllStates((prev) => ({ ...prev, hamMenu: false }));
         document.body.style.overflowY = "visible";
       }}
     ></div>
