bb5eae9c7055d3a6b34f47bd32f8c37a54460bf9
ERFAN-IAN <120744358+ERFAN-IAN@users.noreply.github.com>
2024-08-03 18:27:19 +0330

    Revert "Reapply "added bookmarking functionality""
    This reverts commit 1c1f9b94c6a5e981542cda1d47a5522fc6849593.

--- app/cars/[id]/page.jsx
+++ app/cars/[id]/page.jsx
@@ -1,26 +1,23 @@
 import SwiperSingleCarPage from "@/components/SwiperSingleCarPage";
 import ContactForm from "@/components/ContactForm";
 import CarDetails from "@/components/CarDetails";
-import CarNavbar from "@/components/CarNavbar";
 const page = async ({ params }) => {
   const id = params.id;
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/cars/${id}`);
   const tj = await data.json();
   const car = tj?.car;
-  return (
-    <div>
-      <CarNavbar />
-      <div className="h-full  md:mt-8 grid grid-cols-10  gap-x-2">
-        <div className="hidden md:block sticky top-[6.3rem] light:bg-white z-20 col-span-3 col-start-8 self-start rounded-xl dark:shadow-2xl">
-          <ContactForm />
-        </div>
-        <div className="col-span-10 md:col-span-7 col-start-1 row-start-1">
-          <SwiperSingleCarPage images={car?.images} type={car?.type} />
 
-          <CarDetails car={car} />
-          <div className=" md:hidden light:bg-white z-20 mt-10">
-            <ContactForm />
-          </div>
+  return (
+    <div className="h-full  md:mt-8 grid grid-cols-10  gap-x-2">
+      <div className="hidden md:block sticky top-[6.3rem] light:bg-white z-20 col-span-3 col-start-8 self-start rounded-xl dark:shadow-2xl">
+        <ContactForm />
+      </div>
+      <div className="col-span-10 md:col-span-7 col-start-1 row-start-1">
+        <SwiperSingleCarPage images={car?.images} type={car?.type} />
+
+        <CarDetails car={car} />
+        <div className=" md:hidden light:bg-white z-20 mt-10">
+          <ContactForm />
         </div>
       </div>
     </div>
